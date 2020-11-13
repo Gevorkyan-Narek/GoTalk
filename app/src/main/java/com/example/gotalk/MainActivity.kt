80cd8ed63f4ec92004eebd91c3135a8e683dbb44
Gevorkyan-Narek <mrnarek149@gmail.com>
2020-11-13 14:37:04 +0400

    Добавлена регистрация пользователя

--- app/src/main/java/com/example/gotalk/MainActivity.kt
+++ app/src/main/java/com/example/gotalk/MainActivity.kt
@@ -2,15 +2,23 @@ package com.example.gotalk
 
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.example.gotalk.storage.Storage
 import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
 
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         try {
             FirebaseApp.initializeApp(this)
+            auth = FirebaseAuth.getInstance()
         } catch (e: Exception) {
             e.printStackTrace()
         }
@@ -18,8 +26,16 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         supportFragmentManager.beginTransaction().replace(R.id.fragment, SplashScreen()).commit()
 
         Handler {
-            supportFragmentManager.beginTransaction().replace(R.id.fragment, SignInFragment())
-                .commit()
+
+            val currentUser = auth.currentUser
+            if (currentUser != null) {
+                Storage.setPerson(currentUser.displayName, currentUser.photoUrl)
+                supportFragmentManager.beginTransaction().replace(R.id.fragment, ChatFragment())
+                    .commit()
+            } else {
+                supportFragmentManager.beginTransaction().replace(R.id.fragment, SignInFragment())
+                    .commit()
+            }
             true
         }.sendEmptyMessageDelayed(0, 1500)
     }
--- app/src/main/java/com/example/gotalk/SignInFragment.kt
+++ app/src/main/java/com/example/gotalk/SignInFragment.kt
@@ -1,14 +1,16 @@
 package com.example.gotalk
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.gotalk.storage.Storage
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.sign_in.*
 
 
@@ -18,23 +20,29 @@ class SignInFragment : Fragment(R.layout.sign_in), View.OnClickListener {
         private const val GOOGLE_REQ_CODE = 1
     }
 
+    private lateinit var auth: FirebaseAuth
+    private lateinit var gso: GoogleSignInOptions
+    private lateinit var signGoogle: GoogleSignInClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.server_client_id))
+            .requestEmail()
+            .build()
+        signGoogle = GoogleSignIn.getClient(activity!!, gso)
+
+        auth = FirebaseAuth.getInstance()
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val gso: GoogleSignInOptions =
-            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.server_client_id))
-                .requestEmail()
-                .build()
-
-        val signGoogle = GoogleSignIn.getClient(activity!!, gso)
-
 
         signInGoogle.setOnClickListener {
             val intent: Intent = signGoogle.signInIntent
             startActivityForResult(intent, GOOGLE_REQ_CODE)
         }
-        signInApple.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -50,9 +58,6 @@ class SignInFragment : Fragment(R.layout.sign_in), View.OnClickListener {
                     R.id.signInFacebook -> {
                         R.string.sign_in_with_facebook
                     }
-                    R.id.signInApple -> {
-                        R.string.sign_in_with_apple
-                    }
                     else -> R.string.sign_in
                 }
             )
@@ -64,15 +69,28 @@ class SignInFragment : Fragment(R.layout.sign_in), View.OnClickListener {
             ?.replace(R.id.fragment, fragment)?.commit()
     }
 
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(activity!!) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    Storage.setPerson(user?.displayName, user?.photoUrl)
+                    fragmentManager?.beginTransaction()?.addToBackStack("signInSocial")
+                        ?.replace(R.id.fragment, SignUpProfile())?.commit()
+                } else {
+                    Log.w("AuthWithGoogle", "signInWithCredential:failure", task.exception)
+                }
+            }
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         when (requestCode) {
             GOOGLE_REQ_CODE -> {
                 val task = GoogleSignIn.getSignedInAccountFromIntent(data)
                 if (task.isSuccessful) {
                     val account = task.result!!
-                    Storage.setPerson(account.displayName, account.photoUrl)
-                    fragmentManager?.beginTransaction()?.addToBackStack("signInSocial")
-                        ?.replace(R.id.fragment, SignUpProfile())?.commit()
+                    firebaseAuthWithGoogle(account.idToken!!)
                 } else {
                     Log.d("ActivityResult", task.exception.toString())
                 }
